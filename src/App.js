effb3fbe3178b788c6d0c8e08ca0ac7a968a3fd6
Waqarmemon001 <itx.vicky.o@gmail.com>
2023-07-16 15:55:28 +0500

    creating birthhday reminder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import List from "./componant/List";
+import data from "./data";
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="section">
+          <h3> {people.length} Birthday Today</h3>
+          <List people={people} />
+          <button  onClick={() => setPeople([])}>
+            Clear All
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
